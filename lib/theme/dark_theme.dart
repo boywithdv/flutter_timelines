7bbc9c8ed92d2c120a151d2bfc8a11a9fa8e710f
jukiya <jukiya8891@gmail.com>
2024-02-27 17:12:16 +0900

    キーボード立ち上げ時のエラー原因を消す

--- lib/theme/dark_theme.dart
+++ lib/theme/dark_theme.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 
 ThemeData darkTheme = ThemeData(
-  brightness: Brightness.dark,
-  appBarTheme: const AppBarTheme(
-    backgroundColor: Colors.black,
-  ),
-  colorScheme: ColorScheme.dark(
-    background: Colors.black,
-    primary: Colors.grey[900]!,
-    secondary: Colors.grey[800]!,
-  ),
-  textButtonTheme: TextButtonThemeData(
-    style: TextButton.styleFrom(foregroundColor: Colors.white),
-  ),
-);
+    brightness: Brightness.dark,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.black,
+    ),
+    colorScheme: ColorScheme.dark(
+      background: Colors.black,
+      primary: Colors.grey[900]!,
+      secondary: Colors.grey[800]!,
+    ),
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+        foregroundColor: Colors.white,
+      ),
+    ),
+    primarySwatch: Colors.deepPurple);
